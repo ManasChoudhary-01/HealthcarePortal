2f887cab4cbab96670b5759f175380af99b69bc6
Manas <manasck14@gmail.com>
2025-06-23 17:46:11 +0530

    optimization: reduced pdf file size by converting png to jpeg

--- src/utils/pdfgenerator.jsx
+++ src/utils/pdfgenerator.jsx
@@ -2,14 +2,17 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 export const downloadPDF = async (elementRef, filename = "Prescription.pdf") => {
-  const canvas = await html2canvas(elementRef.current);
-  const imgData = canvas.toDataURL("image/png");
+  const canvas = await html2canvas(elementRef.current,{
+    scale: 2,
+    useCORS: true,
+  });
+  const imgData = canvas.toDataURL("image/jpeg", 1.0);
 
   const pdf = new jsPDF();
   const imgProps = pdf.getImageProperties(imgData);
   const pdfWidth = pdf.internal.pageSize.getWidth();
   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-  pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+  pdf.addImage(imgData, "JPEG", 0, 0, pdfWidth, pdfHeight);
   pdf.save(filename);
 };
