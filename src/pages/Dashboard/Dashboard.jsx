348fd1b693600109216dcab9374b8f50281edba6
Manas <manasck14@gmail.com>
2025-07-27 11:28:17 +0530

    feat: dynamically set dashboard heading based on user role

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,31 +4,60 @@ import { Link } from "react-router-dom";
 
 import RoleWrapper from "../../helper/RoleWrapper";
 import Navbar from "../../components/Header/Navbar";
+import useAuthStore from "../../context/useAuthStore";
 
 export default function Dashboard() {
+
+    const role = useAuthStore((state) => state.role);
+    const [heading, setHeading] = useState("");
+
+    if (role === "RECEPTIONIST") {
+        setHeading("Receptionist Dashboard");
+    } else if (role === "DOCTOR") {
+        setHeading("Doctor Dashboard");
+    } else if (role === "PHARMACIST") {
+        setHeading("Pharmacist Dashboard");
+    } else if (role === "LAB_TECHNICIAN") {
+        setHeading("Lab Technician Dashboard");
+    } else {
+        setHeading("Dashboard");
+    }
 
     return (
         <div className={styles.dashboardContainer}>
             <Navbar />
 
-            <RoleWrapper allowedRoles={["RECEPTIONIST"]}>
-                <h2>Receptionist Dashboard</h2>
-                <div className={styles.wrapper}>
+            <h2>{heading}</h2>
+            <div className={styles.wrapper}>
+
+                <RoleWrapper allowedRoles={["RECEPTIONIST"]}>
                     <Link to="/reception_register">
                         <div className={styles.dashboardItem}>
                             <h3>Register a New Patient</h3>
                             <p>Quickly add a new patient to the system with all necessary details.</p>
                         </div>
                     </Link>
+                </RoleWrapper>
 
+                <RoleWrapper allowedRoles={["RECEPTIONIST", "DOCTOR"]}>
                     <Link to="/appointment">
                         <div className={styles.dashboardItem}>
                             <h3>Book an appointment</h3>
                             <p>Schedule a new appointment for an existing patient or a new one.</p>
                         </div>
                     </Link>
-                </div>
-            </RoleWrapper>
+                </RoleWrapper>
+
+                <RoleWrapper allowedRoles={["DOCTOR"]}>
+                    <Link to="/queue">
+                        <div className={styles.dashboardItem}>
+                            <h3>View Appointment Queue</h3>
+                            <p>Check and manage all upcoming patient appointments in real-time.</p>
+                        </div>
+                    </Link>
+                </RoleWrapper>
+            </div>
+
         </div>
     )
 }
